d1ed8b9c514307b12d7e9c9c2208fe370bcad2a3
MonstersBY <monsters.alex0@gmail.com>
2024-01-10 14:00:44 +0300

    fix: font

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import "./index.scss";
 import "./js/pages/home";
 import "./js/components/header";
 import { example } from "./js/utils/constants";
+import 'swiper/css';
+import "./js/components/baner";
