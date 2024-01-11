2550d79d4a34c4ef2ac65230e07d7948c86d9270
Anastasia <anstasa.li@gmail.com>
2024-01-11 16:24:03 +0400

    feat add aside blocks

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ import { example } from "./js/utils/constants";
 import 'swiper/css';
 import "swiper/css/bundle";
 import "swiper/css/mousewheel";
+import "./js/components/main-a";
 import "./js/components/baner";
---
+++ src/js/components/main-a.js
@@ -0,0 +1,53 @@
+"use strict";
+import $ from "jquery";
+
+let scrollY = 0;
+
+function openModal() {
+  scrollY = window.scrollY;
+  const body = document.body;
+  body.style.height = "100vh";
+  body.style.overflowY = "hidden";
+  if (window.innerWidth > 768) {
+    body.style.paddingRight = "15px";
+  }
+}
+
+function closeModal() {
+  const body = document.body;
+  body.style.position = "";
+  body.style.top = "";
+  body.style.height = "";
+  body.style.overflowY = "";
+  body.style.paddingRight = "";
+  window.history.replaceState(
+    null,
+    null,
+    window.location.pathname + window.location.search,
+  );
+  window.scrollTo(0, scrollY);
+}
+
+$(function () {
+  if ($(window).width() > 768) {
+    $('.statia-aside.modal').hide();
+
+    setTimeout(function () {
+      $('.statia-aside.modal').show();
+      $('.statia-aside.modal').addClass('active');
+      openModal();
+    }, 6000);
+
+    $('.statia-aside__close').on('click', function () {
+      if ($('.statia-aside.modal').is(":visible")) {
+        $('.statia-aside.modal').removeClass('active');
+        closeModal();
+        setTimeout(function () {
+          $('.statia-aside.modal').hide();
+        }, 500)
+      } else {
+        $('.statia-aside.left').addClass('hide');
+      }
+    });
+  }
+});
