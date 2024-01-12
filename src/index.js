77c7f7e08265b33e4f957d88d79267cf9fc4a6e5
Gleb Leus <lv.gleb44@gmail.com>
2024-01-12 16:25:30 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,20 +5,10 @@ import { example } from "./js/utils/constants";
 import "swiper/css";
 import "swiper/css/bundle";
 import "swiper/css/mousewheel";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import "./js/components/main-a";
 import "./js/components/baner";
 import "./js/components/lkChangingBlocks";
-=======
 import "./js/components/lkChangingBlocks";
 import "./js/components/baner";
->>>>>>> 7b949b5 (pages: lk)
-=======
 import "./js/components/lkChangingBlocks";
 import "./js/components/baner";
-<<<<<<< HEAD
->>>>>>> 7b949b5 (pages: lk)
-=======
->>>>>>> 7b949b5378682d5960717e754393ebddadc071d7
->>>>>>> 472a0f9f06d92b6a09a496caa4f66a17eef5235a
