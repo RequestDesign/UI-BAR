09fac0ceeb74f656cca9eac7457cee5d288e75ea
Gleb Leus <lv.gleb44@gmail.com>
2024-01-12 13:00:15 +0300

    pages: lk

--- src/index.js
+++ src/index.js
@@ -6,9 +6,14 @@ import "swiper/css";
 import "swiper/css/bundle";
 import "swiper/css/mousewheel";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import "./js/components/main-a";
 import "./js/components/baner";
 import "./js/components/lkChangingBlocks";
+=======
+import "./js/components/lkChangingBlocks";
+import "./js/components/baner";
+>>>>>>> 7b949b5 (pages: lk)
 =======
 import "./js/components/lkChangingBlocks";
 import "./js/components/baner";
